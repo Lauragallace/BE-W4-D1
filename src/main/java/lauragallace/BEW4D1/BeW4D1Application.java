9c27eac2f50f1269c63c137caaab0804a72d3190
Lauragallace <143189799+Lauragallace@users.noreply.github.com>
2024-01-08 16:03:55 +0100

    first commit

---
+++ src/main/java/lauragallace/BEW4D1/BeW4D1Application.java
@@ -0,0 +1,13 @@
+package lauragallace.BEW4D1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeW4D1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeW4D1Application.class, args);
+	}
+
+}
---
+++ src/test/java/lauragallace/BEW4D1/BeW4D1ApplicationTests.java
@@ -0,0 +1,13 @@
+package lauragallace.BEW4D1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeW4D1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
